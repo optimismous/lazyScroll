3b600af9534bc71e8de1795c69c85c6a0611ac08
Igor Grekhov <igor.24ds@yandex.ru>
2018-07-15 21:25:45 +0300

    render some invisible items, remove unused code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { LazyScroll } from './components/lazy-scroll';
 import { throttle } from 'lodash';
-import './App.css';
 
 function renderItem(props, idx) {
   return <div key={`item-${idx}`} style={{boxSizing: 'border-box', border: '1px solid red', height: 20, lineHeight: '20px', backgroundColor: 'pink', color: 'red'}}>{props.name}</div>;
@@ -11,30 +10,23 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      scrollTop: 0,
-      // todo remove
-      startItemIdx: 5
+      scrollTop: 0
     }
-
-    // window.setInterval(() => {
-    //   this.setState({
-    //     startItemIdx: Math.round(Math.random() * 100) % 2 ? 10 : 5
-    //   });
-    // }, 500);
     this.container = React.createRef();
   }
 
   onScroll = throttle(() => {
-    // console.log('onScroll', this.container.current.scrollTop);
     this.setState({
       scrollTop: this.container.current.scrollTop || 0
     });
+
   }, 300);
 
   render() {
     const items = [];
+    const containerHeight = 500;
     
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 20000; i++) {
       items.push({
         name: `item-${i}`
       });
@@ -42,8 +34,14 @@ class App extends Component {
 
     return (
       <div className="App">
-        <div ref={this.container} onScroll={this.onScroll} style={{height: 500, backgroundColor: 'gray', overflow: 'auto'}}>
-          <LazyScroll items={items} itemHeight={20} renderItem={renderItem} visibleHeight={500} scrollTop={this.state.scrollTop} />
+        <div ref={this.container} onScroll={this.onScroll} style={{height: containerHeight, backgroundColor: 'gray', overflow: 'auto'}}>
+          <LazyScroll
+            items={items}
+            itemHeight={20}
+            renderItem={renderItem}
+            visibleHeight={containerHeight}
+            additionalItems={15}
+            scrollTop={this.state.scrollTop} />
         </div>
       </div>
     );
--- src/components/lazy-scroll.js
+++ src/components/lazy-scroll.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 export class LazyScroll extends React.Component {
 	render() {
-		const { items, itemHeight, renderItem, visibleHeight, scrollTop } = this.props;
-
-		const startIdx = Math.floor(scrollTop / itemHeight);
-		const endIdx = Math.min(startIdx + Math.ceil(visibleHeight / itemHeight), items.length);
+		const { items, itemHeight, renderItem, visibleHeight, scrollTop, additionalItems } = this.props;
+		const topVisibleIndex = Math.floor(scrollTop / itemHeight);
+		const startIdx = Math.max(topVisibleIndex - additionalItems, 0);
+		const endIdx = Math.min(topVisibleIndex + Math.ceil(visibleHeight / itemHeight) + additionalItems, items.length);
 
 		const itemsToRender = items.slice(startIdx, endIdx);
 		const height = items.length * itemHeight;
 		const top = startIdx * itemHeight;
+		
 		return (
 			<div style={{height: height, position: 'relative' }}>
-				<div style={{position: 'absolute', top}}>
+				<div style={{position: 'absolute', left: 0, right: 0, top}}>
 					{itemsToRender.map(renderItem)}
 				</div>
 			</div>
